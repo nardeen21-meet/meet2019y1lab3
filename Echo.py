f35f9ff67dca5edd4f5efcda0eab0bf2bf5346ff
nardeen21-meet <nardeen21@meet.mit.edu>
2019-07-23 12:34:49 +0300

    Stopping here

---
+++ Echo.py
@@ -0,0 +1,9 @@
+user= input ("say something! ")
+print(user)
+respond= input ("hello")
+print(respond.upper())
+respond= input ("how are you")
+print (respond.lower())
+respond= input ("can you hear me?")
+print (respond.swapcase())
+       
---
+++ greeting.py
@@ -0,0 +1,6 @@
+user=input ("whats your name")
+print (user)
+user= input ("hello there "+ user.capitalize())
+print (user)
+print("your name is "+str (len("nardeen"))+ " letters long")
+
